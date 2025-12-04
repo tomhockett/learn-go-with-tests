3cb48f5c8015c61ae5f57c7621dab66bf6d01a3e
Tom Hockett <tom.hockett@gmail.com>
2025-12-03 18:29:57 -0800

    add integer section

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/tomhockett/learn-go-with-tests
+
+go 1.25.5
---
+++ integers/adder.go
@@ -0,0 +1,7 @@
+// Package integers provides basic integer operations.
+package integers
+
+// Add returns the sum of two integers.
+func Add(x, y int) int {
+	return x + y
+}
