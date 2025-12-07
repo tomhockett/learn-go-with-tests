50e703a834dbdf866b60f2d1ddf0cb2c846e2513
Tom Hockett <tom.hockett@gmail.com>
2025-12-07 10:53:21 -0800

    update iteration

--- iteration/iteration.go
+++ iteration/iteration.go
@@ -1,12 +1,23 @@
 // Package iteration repeats a character multiple times.
 package iteration
 
+import "strings"
+
 const repeatCount = 5
 
-func Repeat(character string) string {
-	var repeated string
-	for i := 0; i < repeatCount; i++ {
-		repeated += character
+// Strings in Go are immutable. So we can't modify them in place.
+//func Repeat(character string) string {
+//	var repeated string
+//	for i := 0; i < repeatCount; i++ {
+//		repeated += character
+//	}
+//	return repeated
+//}
+
+func Repeat(character string, times int) string {
+	var repeated strings.Builder
+	for i := 0; i < times; i++ {
+		repeated.WriteString(character)
 	}
-	return repeated
+	return repeated.String()
 }
--- iteration/iteration_test.go
+++ iteration/iteration_test.go
@@ -1,12 +1,25 @@
 package iteration
 
 import "testing"
+import "fmt"
 
 func TestRepeat(t *testing.T) {
-	repeated := Repeat("a")
-	expected := "aaaaa"
+	repeated := Repeat("a", 9)
+	expected := "aaaaaaaaa"
 
 	if repeated != expected {
 		t.Errorf("expected %q but got %q", expected, repeated)
 	}
+}
+
+func BenchmarkRepeat(b *testing.B) {
+	for b.Loop() {
+		Repeat("a", 9)
+	}
+}
+
+func ExampleRepeat() {
+	repeated := Repeat("a", 9)
+	fmt.Println(repeated)
+	// Output: aaaaaaaaa
 }
