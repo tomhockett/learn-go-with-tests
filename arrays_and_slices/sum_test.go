a638cdf3e5142b69d8875b9a1d54d0599413cdb1
Tom Hockett <tom.hockett@gmail.com>
2025-12-07 11:02:13 -0800

    add SumAll public function

--- arrays_and_slices/sum_test.go
+++ arrays_and_slices/sum_test.go
@@ -1,6 +1,9 @@
 package main
 
-import "testing"
+import (
+	"slices"
+	"testing"
+)
 
 func TestSum(t *testing.T) {
 
@@ -15,4 +18,14 @@ func TestSum(t *testing.T) {
 		}
 	})
 
+}
+
+func TestSumAll(t *testing.T) {
+
+	got := SumAll([]int{1, 2}, []int{0, 9})
+	want := []int{3, 9}
+
+	if !slices.Equal(got, want) {
+		t.Errorf("got %v want %v", got, want)
+	}
 }
