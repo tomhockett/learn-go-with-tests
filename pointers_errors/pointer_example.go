1a7f2a5d5b4b1e50df308003d6959e0548b3c994
Tom Hockett <tom.hockett@gmail.com>
2025-12-14 13:32:27 -0800

    update pointer example

--- pointers_errors/pointer_example.go
+++ pointers_errors/pointer_example.go
@@ -1,12 +1,18 @@
+// Package main will show how pointers work in contrast to values with 2 functions: zeroval and zeroptr.
 package main
 
 import "fmt"
 
+// zeroval has an int parameter, so arguments will be passed to it by value.
 func zeroval(ival int) {
+	// zeroval will get a copy of ival distinct from the one in the calling function.
 	ival = 0
 }
 
+// zeroptr, in contrast, has an *int parameter, meaning that it takes an int pointer.
 func zeroptr(iptr *int) {
+	// The *iptr code in the function body then dereferences the pointer from its memory address to the current value at that address.
+	// Assigning a value to a dereferenced pointer changes the value at the referenced address.
 	*iptr = 0
 }
 
@@ -14,11 +20,14 @@ func main() {
 	i := 1
 	fmt.Println("initial:", i)
 
+	// zeroval doesn’t change the i in main, but zeroptr does because it has a reference to the memory address for that variable.
 	zeroval(i)
 	fmt.Println("zeroval:", i)
 
+	// The &i syntax gives the memory address of i, i.e. a pointer to i.
 	zeroptr(&i)
 	fmt.Println("zeroptr:", i)
 
+	// Pointers can be printed too.
 	fmt.Println("pointer:", &i)
 }
