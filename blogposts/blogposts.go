35256a7a85def6398d5170573f1db70058e1a93c
Tom Hockett <tom.hockett@gmail.com>
2026-01-07 21:34:59 -0800

    Extract metadata (title, description, tags) and body from post content in NewPostsFromFS, refactor code, and update tests.

--- blogposts/blogposts.go
+++ blogposts/blogposts.go
@@ -1,14 +1,10 @@
 package blogposts
 
 import (
-	"io"
 	"io/fs"
 )
 
-type Post struct {
-	Title string
-}
-
+// NewPostsFromFS returns a collection of blog posts from a file system. If it does not conform to the format then it'll return an error
 func NewPostsFromFS(fileSystem fs.FS) ([]Post, error) {
 	dir, err := fs.ReadDir(fileSystem, ".")
 	if err != nil {
@@ -16,7 +12,7 @@ func NewPostsFromFS(fileSystem fs.FS) ([]Post, error) {
 	}
 	var posts []Post
 	for _, f := range dir {
-		post, err := getPost(fileSystem, f.Name())
+		post, err := getPost(fileSystem, f)
 		if err != nil {
 			return nil, err //todo: needs clarification, should we totally fail if one file fails? or just ignore?
 		}
@@ -25,21 +21,12 @@ func NewPostsFromFS(fileSystem fs.FS) ([]Post, error) {
 	return posts, nil
 }
 
-func getPost(fileSystem fs.FS, fileName string) (Post, error) {
-	postFile, err := fileSystem.Open(fileName)
+func getPost(fileSystem fs.FS, f fs.DirEntry) (Post, error) {
+	postFile, err := fileSystem.Open(f.Name())
 	if err != nil {
 		return Post{}, err
 	}
 	defer postFile.Close()
+
 	return newPost(postFile)
-}
-
-func newPost(postFile io.Reader) (Post, error) {
-	postData, err := io.ReadAll(postFile)
-	if err != nil {
-		return Post{}, err
-	}
-
-	post := Post{Title: string(postData)[7:]}
-	return post, nil
 }
--- blogposts/blogposts_test.go
+++ blogposts/blogposts_test.go
@@ -1,8 +1,6 @@
 package blogposts_test
 
 import (
-	"errors"
-	"io/fs"
 	"reflect"
 	"testing"
 	"testing/fstest"
@@ -10,36 +8,60 @@ import (
 	"github.com/tomhockett/learn-go-with-tests/blogposts"
 )
 
-type StubFailingFS struct {
-}
-
-func (s StubFailingFS) Open(name string) (fs.File, error) {
-	return nil, errors.New("oh no, i always fail")
-}
-
 func TestNewBlogPosts(t *testing.T) {
+	const (
+		firstBody = `Title: Post 1
+Description: Description 1
+Tags: tdd, go
+---
+Hello
+World`
+		secondBody = `Title: Post 2
+Description: Description 2
+Tags: rust, borrow-checker
+---
+B
+L
+M`
+	)
+
 	fs := fstest.MapFS{
-		"hello world.md":  {Data: []byte("Title: Post 1")},
-		"hello-world2.md": {Data: []byte("Title: Post 2")},
+		"hello world.md":  {Data: []byte(firstBody)},
+		"hello-world2.md": {Data: []byte(secondBody)},
 	}
 
 	posts, err := blogposts.NewPostsFromFS(fs)
 
+	assertNoError(t, err)
+
+	assertPostsLength(t, posts, fs)
+
+	assertPost(t, posts[0], blogposts.Post{
+		Title:       "Post 1",
+		Description: "Description 1",
+		Tags:        []string{"tdd", "go"},
+		Body: `Hello
+World`,
+	})
+}
+
+func assertNoError(t *testing.T, err error) {
+	t.Helper()
 	if err != nil {
 		t.Fatal(err)
 	}
+}
 
-	got := posts[0]
-	want := blogposts.Post{Title: "Post 1"}
-
-	if !reflect.DeepEqual(got, want) {
-		t.Errorf("got %+v, want %+v", got, want)
+func assertPostsLength(t *testing.T, posts []blogposts.Post, fs fstest.MapFS) {
+	t.Helper()
+	if len(posts) != len(fs) {
+		t.Errorf("got %d posts, wanted %d posts", len(posts), len(fs))
 	}
 }
 
-func TestNewBlogPosts_fails(t *testing.T) {
-	_, err := blogposts.NewPostsFromFS(StubFailingFS{})
-	if err == nil {
-		t.Fatal("expected error but got none")
+func assertPost(t *testing.T, got blogposts.Post, want blogposts.Post) {
+	t.Helper()
+	if !reflect.DeepEqual(got, want) {
+		t.Errorf("got %+v, want %+v", got, want)
 	}
 }
