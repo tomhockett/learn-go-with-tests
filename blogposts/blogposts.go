35152cc23a325cfef577d4da33abd9dd0e6a4b58
Tom Hockett <tom.hockett@gmail.com>
2026-01-07 21:12:08 -0800

    Handle errors in NewPostsFromFS and add test coverage for failure cases

--- blogposts/blogposts.go
+++ blogposts/blogposts.go
@@ -7,11 +7,14 @@ import (
 type Post struct {
 }
 
-func NewPostsFromFS(fileSystem fs.FS) []Post {
-	dir, _ := fs.ReadDir(fileSystem, ".")
+func NewPostsFromFS(fileSystem fs.FS) ([]Post, error) {
+	dir, err := fs.ReadDir(fileSystem, ".")
+	if err != nil {
+		return nil, err
+	}
 	var posts []Post
 	for range dir {
 		posts = append(posts, Post{})
 	}
-	return posts
+	return posts, nil
 }
