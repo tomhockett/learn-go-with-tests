0598e8e0fc87512f3363a0cc609e7a99e44d530e
Tom Hockett <tom.hockett@gmail.com>
2026-01-09 15:57:04 -0800

    Add blogrenderer package with HTML rendering for posts and basic tests

---
+++ blogrenderer/renderer.go
@@ -0,0 +1,30 @@
+package blogrenderer
+
+import (
+	"html/template"
+	"io"
+)
+
+const (
+	postTemplate = `<h1>{{.Title}}</h1><p>{{.Description}}</p>Tags: <ul>{{range .Tags}}<li>{{.}}</li>{{end}}</ul>`
+)
+
+type Post struct {
+	Title       string
+	Body        string
+	Description string
+	Tags        []string
+}
+
+func Render(w io.Writer, p Post) error {
+	templ, err := template.New("blog").Parse(postTemplate)
+	if err != nil {
+		return err
+	}
+
+	if err := templ.Execute(w, p); err != nil {
+		return err
+	}
+
+	return nil
+}
