4c2585880e7583e6aba03e244061003f4826ad37
Tom Hockett <tom.hockett@gmail.com>
2026-01-11 19:48:17 -0800

    Add `Stack` implementation with generics and corresponding tests

---
+++ generics/assert.go
@@ -0,0 +1,43 @@
+package generics
+
+import "testing"
+
+func TestAssertFunctions(t *testing.T) {
+	t.Run("asserting on integers", func(t *testing.T) {
+		AssertEqual(t, 1, 1)
+		AssertNotEqual(t, 1, 2)
+	})
+
+	t.Run("asserting on strings", func(t *testing.T) {
+		AssertEqual(t, "hello", "hello")
+		AssertNotEqual(t, "hello", "Grace")
+	})
+}
+
+func AssertEqual(t *testing.T, got, want interface{}) {
+	t.Helper()
+	if got != want {
+		t.Errorf("got %d, want %d", got, want)
+	}
+}
+
+func AssertNotEqual(t *testing.T, got, want interface{}) {
+	t.Helper()
+	if got == want {
+		t.Errorf("didn't want %d", got)
+	}
+}
+
+func AssertTrue(t *testing.T, got bool) {
+	t.Helper()
+	if !got {
+		t.Errorf("got %v, want true", got)
+	}
+}
+
+func AssertFalse(t *testing.T, got bool) {
+	t.Helper()
+	if got {
+		t.Errorf("got %v, want false", got)
+	}
+}
